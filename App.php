7b1f1a561a163e58a48209ccd8774dd5f8d19d80
ifsale <carlo@salapc.com>
2016-01-06 17:21:14 +0100

    new release

---
+++ App.php
@@ -0,0 +1,356 @@
+<?php
+
+	/**
+	* Application Interface
+	* @package	FluidPhp MVC
+	* @author	Carlo Pietrobattista
+	*/
+	
+	namespace fluidphp\framework;
+	
+	use PtcHandyMan as HandyMan;
+	use PtcDebug as Debug;
+	use PtcRouter as Router;
+	use PtcDb as DB;
+	use PtcEvent as Event;
+	use fluidphp\framework\Module\Manager as Module;
+
+	class App
+	{
+		/**
+		* Application start event
+		* @param	mixed	$callback		a valid callback
+		*/
+		public static function start( $callback ){ return ptc_listen( 'app.start' , $callback ); }
+		/**
+		* Application stop event
+		* @param	mixed	$callback		a valid callback
+		*/
+		public static function stop( $callback ){ return ptc_listen( 'app.stop' , $callback ); }
+		/**
+		*
+		*/
+		public function __construct( )
+		{
+			$class = get_called_class( );
+			$event = Event::getEvent( 'app' );
+			if ( is_array( $event ) && ptc_array_get( $event , 'start' , false ) )
+			{ 
+				ptc_fire( 'app.start' , array( &$class::$_config , &$this ) ); 
+			}
+			$this->_appConfig = $class::$_config;
+			ptc_log( $this->_appConfig , 'Started the application' , 'App Config' );
+		}
+		/**
+		*
+		*/
+		public function run( $print = true )
+		{
+			$check_config = ptc_array_get( $this->_appConfig , 'app.check_router_config' );
+			$this->_modules = Module::all( );
+			$uri = Router::getUri( );
+			$location = Module::location( );
+			$raw = str_replace( \App::option( 'modules.env' ) , '' , $uri[ 'path' ] );
+			if ( $location && 0 === strpos( $raw , $location ) )
+			{
+				$raw = substr( $raw , strlen( $location ) );
+				$uri[ 'path' ] = \App::option( 'modules.env' ) . $raw;
+			}
+			$response = Router::run( $check_config , $uri ); 
+			$class = get_called_class( );
+			return $class::_shutdown( $response , $print , $this );
+		}
+		/**
+		*
+		*/
+		public function cli( $arguments , $print = true )
+		{
+			$flags = getopt( 'm:c:' );
+			if ( !isset( $flags[ 'c' ] ) )
+			{
+				throw new \Exception( 'Please use the "-c" flag followed by the process name' );
+			}
+			foreach ( $flags as $k => $v )
+			{	
+				unset( $arguments[ array_search( '-' . $k , $arguments ) ] );
+				unset( $arguments[ array_search( $v , $arguments ) ] );
+			}
+			unset( $arguments[ 0 ] );
+			$arguments = array_values( $arguments );
+			$response = Cli::run( $flags[ 'c' ] , $arguments ); 
+			$class = get_called_class( );
+			return $class::_shutdown( $response , $print , $this );
+		}
+		/**
+		*
+		*/
+		public static function env( )
+		{
+			return $env = ( '/' === substr( App::option( 'app.env' ) , -1 ) ) ? 
+					substr( App::option( 'app.env' ) , 0 , -1 ) : App::option( 'app.env' );
+		}
+		/**
+		* Alias of @ref App::options( )
+		*/
+		public static function option( $key = null , $option = null )
+		{ 
+			return static::options( $key , $option );
+		}
+		/**
+		*
+		*/
+		public static function options( $key = null , $options = null )
+		{
+			if ( is_null( $options ) )
+			{
+				if ( !$key ){ return static::$_config; }
+				return ptc_array_get( static::$_config , $key );
+			}
+			else if ( ptc_array_get( static::$_config , $key ) )
+			{
+				trigger_error( 'Option name ' . $key . ' already exists!' , E_USER_ERROR );
+				return false;
+			}
+			return ptc_array_set( static::$_config , $key , $options );
+		}
+		/**
+		*
+		*/
+		public static function storage( $key = null , $value = null )
+		{
+			if ( is_null( $value ) )
+			{
+				if ( !$key ){ return static::$_storage; }
+				return ptc_array_get( static::$_storage , $key );
+			}
+			else if ( ptc_array_get( static::$_storage , $key ) )
+			{
+				trigger_error( 'Storage name ' . $key . ' already exists!' , E_USER_ERROR );
+				return false;
+			}
+			return ptc_array_set( static::$_storage , $key , $value );
+		}
+		/**
+		*
+		*/
+		public static function configure( )
+		{
+			static::_registerModules( );
+			static::_setPhpInit( );
+			static::_setDebug( );
+			static::_setAppConfig( );
+			static::_setCustomConfigFiles( );
+		}
+		/**
+		*
+		*/
+		protected $_appConfig = array( );
+		/**
+		*
+		*/
+		protected $_modules = array( );
+		/**
+		*
+		*/
+		protected static $_config = array( );
+		/**
+		*
+		*/
+		protected static $_storage = array( );
+		/**
+		*
+		*/
+		protected static $_debugLevels = array
+		( 
+			'only_system_actions'	=>	array
+			(
+				'Autoloader Config' , 'Paths Manager' , 'Router Config' , 'App Config' ,
+				'View Config' , 'Translator Helper Config' , 'Website Helper Config' ,
+				'StreamWrapper Config' , 'Module Manager Config'
+			) ,
+			'no_system_messages'	=>	array
+			(
+				'Router Action' , 'View Config' , 'View Action' , 
+				'Autoloader Config' , 'Autoloader Action' ,'Paths Manager' , 
+				'App Config' , 'Website Helper Config' , 'Website Helper Action' , 
+				'Router Config' , 'Translator Helper Config' , 'Session Manager' ,
+				'Event Manager' , 'StreamWrapper Action' , 'StreamWrapper Config' , 
+				'Modules Manager Config' , 'Modules Manager Action'
+			)
+		);
+		/**
+		*
+		*/
+		protected static function _shutdown( $response , $print , $obj )
+		{
+			$event = Event::getEvent( 'app');
+			if ( is_array( $event ) && ptc_array_get( $event , 'stop' , false ) )
+			{ 
+				ptc_fire( 'app.stop' , array( $obj , &$response ) ); 
+			}
+			if ( !$print ){ return $response; }  
+			echo $response;
+		}
+		/**
+		* Php configuration at runtime
+		*/
+		protected static function _setPhpInit( )
+		{
+			$init = Module::merge( ptc_path( 'root' ) . '/app/config/init.php' );
+			foreach ( $init as $k => $v )
+			{
+				if ( is_array( $v ) )
+				{
+					foreach ( $v as $key => $val )
+					{
+						ini_set( $k . '.' . $key , $val );
+					}
+				}
+				else
+				{
+					ini_set( $k , $v );
+				}
+			}
+			static::option( 'init' , $init );
+		}
+		/**
+		* Set debug at runtime
+		*/
+		protected static function _setDebug( )
+		{
+			$debug =  Module::merge( ptc_path( 'root' ) . '/app/config/debug.php' ); 
+			if ( file_exists( ptc_path( 'root' ) . '/app/config/debug_ajax.php' ) )
+			{
+				if ( !empty( $_SERVER[ 'HTTP_X_REQUESTED_WITH' ] ) && 
+					( 'xmlhttprequest' === strtolower( $_SERVER[ 'HTTP_X_REQUESTED_WITH' ] ) || 
+								'ajax' === strtolower( $_SERVER[ 'HTTP_X_REQUESTED_WITH' ]) ) ) 
+				{
+					$debug_ajax =  require_once( ptc_path( 'root' ) . '/app/config/debug_ajax.php' ); 
+					$debug = array_merge( $debug , $debug_ajax );
+				}
+			}
+			static::option( 'debug' , $debug );
+			require_once( ptc_path( 'root' ) . '/vendor/phptoolcase/phptoolcase/PtcDebug.php' );
+			if ( !is_null( ptc_array_get( static::$_config , 'debug.replace_error_handler' , null ) ) )
+			{
+				$die_on_error = ( ptc_array_get( static::$_config , 'debug.die_on_error' ) ) ? true : false;
+				Debug::setErrorHandler( $die_on_error );
+				ptc_array_set( static::$_config , 'debug.replace_error_handler' , false , true );
+			}
+			if ( ptc_array_get( static::$_config , 'debug.start' ) )
+			{ 
+				$exclude = ptc_array_get( static::$_config , 'debug.exclude_categories' );
+				if ( is_string( $exclude ) && array_key_exists( $exclude , static::$_debugLevels ) )
+				{
+					ptc_array_set( static::$_config , 'debug.exclude_categories' , 
+										static::$_debugLevels[ $exclude ], true );
+				}
+				if ( isset( $_GET[ 'debug_level' ] ) )
+				{
+					ptc_array_set( static::$_config , 'debug.exclude_categories' , 
+									static::$_debugLevels[ $_GET[ 'debug_level' ] ] , true );
+				}
+				Debug::load( static::$_config[ 'debug' ] ); 
+			}
+		}
+		/**
+		* Registers the Modules Manager
+		*/
+		protected static function _registerModules( )
+		{
+			static::option( 'modules' , Module::merge( ptc_path( 'root' ) . '/app/config/modules.php' ) );
+			Module::check( );
+		}
+		/**
+		* Application bootstrap configuration
+		*/
+		protected static function _setAppConfig( )
+		{
+			/* paths */
+			static::option( 'paths' , Module::merge( ptc_path( 'root' ) . '/app/config/paths.php' ) );
+			ptc_add_path( ptc_array_get( static::$_config , 'paths' ) );
+			/* application */
+			static::option( 'app' , Module::merge( ptc_path( 'root' ) . '/app/config/app.php' ) );
+			HandyMan::addAlias( ptc_array_get( static::$_config , 'app.aliases' ) );
+			if ( $locale = ptc_array_get( static::$_config , 'app.locale' ) )
+			{ 
+				setlocale( LC_ALL , $locale . 'UTF-8' ); 
+			} 
+			if ( $timezone = ptc_array_get( static::$_config , 'app.timezone' ) )
+			{ 
+				date_default_timezone_set( $timezone ); 
+			}
+			ptc_add_file( ptc_array_get( static::$_config , 'app.files' ) );
+			ptc_add_dir( ptc_array_get( static::$_config , 'app.directories' ) );
+			ptc_add_dir( ptc_array_get( static::$_config , 'app.namespaces' ) );
+			HandyMan::addSeparators( ptc_array_get( static::$_config , 'app.separators' ) );
+			HandyMan::addConventions( ptc_array_get( static::$_config , 'app.conventions' ) );
+			/* database */
+			static::option( 'db' , Module::merge( ptc_path( 'root' ) . '/app/config/db.php' ) );
+			if ( $db = ptc_array_get( static::$_config , 'db' ) )
+			{
+				$loop = ( static::option( 'app.test_env' ) ) ? $db[ 'develop' ] : $db[ 'prod' ];
+				foreach ( $loop as $k => $v )
+				{ 
+					if ( ptc_array_get( $v , 'user' ) )
+					{ 
+						DB::add( $v , $k );
+					}
+				}
+			}
+			/* auth */
+			static::option( 'auth' , Module::merge( ptc_path( 'root' ) . '/app/config/auth.php' ) );
+		}
+		/**
+		* Load custom config files
+		*/
+		protected static function _setCustomConfigFiles( )
+		{	
+			/* custom config files */
+			$files = array( '..' , '.' , 'app.php' , 'db.php' , 'debug.php' , 'init.php' , 'modules.php' ,
+											'paths.php' , 'auth.php' , 'debug_ajax.php' );
+			$scanned_directory = array_diff( scandir( ptc_path( 'root' ) . '/app/config' ) , $files );
+			if ( !empty( $scanned_directory ) )
+			{
+				foreach ( $scanned_directory as $file )
+				{
+					$option_name = str_replace( '.php' , '' , $file );
+					$options = Module::merge( ptc_path( 'root' ) . '/app/config/' . $file );
+					if ( ptc_array_get( $options , '_load' ) )
+					{
+						$options = call_user_func( ptc_array_get( $options , '_load' ) , $options );
+					}
+					static::option( $option_name , $options );
+				}
+			}
+			static::_loadModulesConfig( );
+		}
+		/**
+		* Load module config files that are not in root config
+		*/
+		protected static function _loadModulesConfig( )
+		{
+			$config = array( '..' , '.' );
+			foreach ( static::options( ) as $key => $val )
+			{
+				$config[ ] = $key . '.php';
+			}
+			foreach ( Module::all( ) as $k => $module )
+			{
+				$scanned_directory = array_diff( scandir( ptc_path( 'root' ) . '/modules/' . $k . '/config' ) , $config );
+				if ( !empty( $scanned_directory ) )
+				{
+					foreach ( $scanned_directory as $file )
+					{
+						$option_name = str_replace( '.php' , '' , $file );
+						$options = require( ptc_path( 'root' ) . '/modules/' . $k . '/config/' . $file );
+						if ( ptc_array_get( $options , '_load' ) )
+						{
+							$options = call_user_func( ptc_array_get( $options , '_load' ) , $options );
+						}
+						static::option( $option_name , $options );
+					}
+				}
+			}
+		}
+	}
---
+++ Module/Manager.php
@@ -0,0 +1,181 @@
+<?php
+
+	/**
+	* Modules Manager
+	* @package	FluidPhp MVC
+	* @author	Carlo Pietrobattista
+	*/
+	
+	namespace fluidphp\framework\Module;
+	
+	use fluidphp\framework\App as App;
+	use PtcEvent as Event;
+
+	class Manager
+	{
+		public static function register( )
+		{
+			Event::listen( 'autoloader.realpath' , function( &$path )
+			{
+				$path = str_replace( '//' , '/' , $path );
+				$path = str_replace( array( '/' , '\\' ) , DIRECTORY_SEPARATOR , $path );
+				$path = \system\Core\Module\StreamWrapper::intercept( $path , 'realpath' );
+			} );
+			StreamWrapper::wrap( );
+		}
+		
+		public static function all( )
+		{
+			return static::$_modules;
+		}
+		
+		public static function get( $name )
+		{
+			return static::$_modules[ $name ];
+		}
+		
+		public static function location( )
+		{
+			if ( !static::$_currentLocation ){ return null; }
+			return static::_env( static::$_currentLocation );
+		}
+		
+		public static function domain( )
+		{
+			return static::$_currentDomain;
+		}
+		
+		public static function isLoaded( $name )
+		{
+			return ( array_key_exists( $name , static::$_modules ) ) ? true : false;
+		}
+		
+		public static function merge( $file )
+		{
+			if ( empty( static::$_modules ) )
+			{
+				return require_once( $file );
+			}
+			$option_name = @end( explode( '/' , str_replace( '.php' , '' , $file ) ) );
+			$options = ( file_exists( $file ) ) ? require_once( $file ) : array( );
+			foreach ( static::$_modules as $module )
+			{
+				$m_path = $module->path( );
+				$newfile = str_replace( ptc_path( 'root' ) . 
+							'/app/config' , $m_path . '/config' , $file );	
+				$module_options = @$module->option( $option_name );
+				if ( $newfile != $file && $module_options )
+				{
+					foreach ( $module_options  as $k => $v )
+					{
+						if ( is_array( $v ) && array_key_exists( $k , $options ) )
+						{
+							$val = array_merge( $options[ $k ] , $v );
+							$options[ $k ] = array_unique( $val );
+						}
+						else { $options[ $k ] = $v; }
+					}
+					ptc_log( [ $newfile , $options ] , 'Module "' . $module->name( ) . 
+								'" merged config file!' , 'Modules Manager Action' );
+				}
+			}
+			return $options;
+		}
+		
+		public static function check( )
+		{
+			$options = App::option( 'modules' );
+			$modules = ( $options[ 'test_env' ] ) ? $options[ 'develop' ] : $options[ 'prod' ];
+			if ( 'cli' === PHP_SAPI ) 
+			{
+				$module = getopt( 'm:' );
+				if ( isset( $module[ 'm' ] ) )
+				{	
+					static::_create( $module[ 'm' ] );
+				}
+			}
+			else
+			{
+				if ( isset( $modules[ 'domains' ] ) )
+				{			
+					static::_checkDomains( $modules[ 'domains' ] );
+				}
+				if ( isset( $modules[ 'locations' ] ) && empty( static::$_modules ) )
+				{
+					static::_checkLocations( $modules[ 'locations' ] );
+				}
+			}
+			if ( !empty ( static::$_modules ) ){ static::register( ); }
+		}
+		
+		protected static $_modules = array( );
+		
+		protected static $_currentLocation = null;
+		
+		protected static $_currentDomain = null;
+		
+		protected static function _checkDomains( $domains )
+		{
+			if ( isset( $domains[ '*' ] ) ) // wildcard
+			{
+				static::_create( $domains[ '*' ] );
+			}
+			foreach ( $domains as $k => $v )
+			{
+				if ( $k === $_SERVER[ 'HTTP_HOST' ] )
+				{
+					static::$_currentDomain = $k;
+					static::_create( $v );
+					break; // 1 domain only
+				}
+			}
+		}
+		
+		protected static function _checkLocations( $locations )
+		{
+			$env = App::option( 'modules.env' );
+			$uri = str_replace( $env , '' , $_SERVER[ 'REQUEST_URI' ] );
+			foreach ( $locations as $k => $v )
+			{
+				if ( 0 === strpos( $uri , $k ) )
+				{
+					static::$_currentLocation = $k;
+					static::_create( $v );
+					break; // only 1 location
+				}
+			}
+		}
+		
+		protected static function _create( $modules )
+		{
+			$modules = ( is_array( $modules ) ) ? $modules : array( $modules );
+			foreach ( $modules as $name )
+			{
+				if ( array_key_exists( $name , static::$_modules ) )
+				{
+					trigger_error( 'Module "' . $name . 
+						'" already present!' , E_USER_ERROR );
+					return;
+				}
+				static::_addModule( $name );
+			}
+		}
+		
+		protected static function _addModule( $name )
+		{
+			$path = ptc_path( 'root' ) . '/modules/' .  $name;
+			if ( !realpath( $path ) )
+			{
+				trigger_error( 'Module "' . $name . 
+					'" is not accessible!' , E_USER_ERROR );
+				return;
+			}
+			static::$_modules[ $name ] = new Instance( $name , $path );
+			ptc_add_path( [ 'module_' . $name => $path ] );
+		}
+
+		protected static function _env( $string )
+		{
+			return ( '/' === substr( $string , -1 ) ) ? substr( $string , 0 , -1 ) : $string;
+		}
+	}
